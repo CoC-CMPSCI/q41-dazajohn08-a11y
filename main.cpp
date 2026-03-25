934f9887bcc1f031616b086740496ee93fee3b9b
John <dazajohn08@gmail.com>
2026-03-24 23:18:33 -0700

    #1

--- main.cpp
+++ main.cpp
@@ -19,9 +19,34 @@ int main()
     cin >> choice;
 
     // TODO
+    if (choice == 1) {
+        cout << "Enter the radius of the circle: ";
+        cin >> radius;
+        area = PI * radius * radius;
+        cout << setprecision(2) << fixed;
+        cout << "The area is " << area << endl;
+    }
 
+    else if (choice == 2){
+    cout << "Enter the lenght and width of the rectangle: ";
+    cin >> height >> width;
+    area = height * width;
     cout << left << setprecision(2) << fixed;
     cout << "The area is " << area << endl;
 
+    }
+    else if (choice == 3){
+    cout << "Enter the base and height of the triangle: ";
+    cin >> base >> height;
+    area = height * width * 0.5;
+    cout << left << setprecision(2) << fixed;
+    cout << "The area is " << area << endl;
+    }
+    else if (choice == 4) {
+        cout << "Program Stopped" << endl;
+    }
+    else {
+        cout << "Wrong number" << endl;
+    }
     return 0;
 }
